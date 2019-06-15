e562fb410d042ecf811e41bbb9621d43a46f04df
Marina Lunts <marinalunts@gmail.com>
2019-06-15 14:50:12 +0300

    added function for retrieving additional information from movie id

--- TestCasePr/Service.swift
+++ TestCasePr/Service.swift
@@ -21,7 +21,7 @@ class Service: NSObject {
             if response.result.isSuccess {
                 let json : JSON = JSON(response.result.value!)
                 for i in 0..<json["results"].count {
-                    movies.append(Movie( id: json["results"][i]["id"].intValue, title: json["results"][i]["title"].stringValue, adult: json["results"][i]["adult"].stringValue, overview: json["results"][i]["overview"].stringValue, image: json["results"][i]["poster_path"].stringValue, releaseString: json["results"][i]["release_date"].stringValue, averageVote: json["results"][i]["vote_average"].doubleValue))
+                    movies.append(Movie(id: json["results"][i]["id"].intValue, title: json["results"][i]["title"].stringValue, adult: json["results"][i]["adult"].stringValue, overview: json["results"][i]["overview"].stringValue, image: json["results"][i]["poster_path"].stringValue, releaseString: json["results"][i]["release_date"].stringValue, averageVote: json["results"][i]["vote_average"].doubleValue))
                 }
 
                 completion(movies,true)
@@ -31,6 +31,30 @@ class Service: NSObject {
                 completion([],false)
             }
         }
+    }
+    
+    public func getMovieByID(selectedMovie : Movie, completion: @escaping (Movie,Bool) -> Void) {
+        var movie = selectedMovie
+        let parameters : [String : String] = ["api_key" : API_KEY, "region" : REGION]
+        
+        Alamofire.request("\(MOVIE_URL)\(movie.id)?", method: .get, parameters: parameters).responseJSON {
+            response in
+            if response.result.isSuccess {
+                let json : JSON = JSON(response.result.value!)
+               print("\(IMAGE_URL)\(json["backdrop_path"].stringValue)")
+                movie.runtime = json["runtime"].intValue
+                movie.backgrImage = movie.getImageFromURL(url: "\(IMAGE_URL)\(json["backdrop_path"].stringValue)")
+                movie.tagLine = json["tagline"].stringValue
+                
+                completion(movie,true)
+            }
+            else {
+                print("error")
+                completion(movie,false)
+            }
+        }
+        
+        
     }
     
 }
